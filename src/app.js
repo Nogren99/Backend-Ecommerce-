02b37c9fcaa6f21599c33024961326cbc581ee59
Nogren99 <nogren23@gmail.com>
2023-02-24 21:07:44 -0300

    handlebars

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 import productsRouter from './routes/products.router.js'
 import cartRouter from './routes/cart.router.js'
+import __dirname from './utils.js';
+import viewsRouter from './routes/views.router.js'
 
 
 const app = express();
+
+app.engine('handlebars',handlebars.engine());
+app.set('views',`${__dirname}/views`)
+app.set('view engine','handlebars ')
+
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api/products',productsRouter);
 app.use('/api/carts',cartRouter);
-
+app.use('/',viewsRouter)
 
 
 app.listen(8080,()=>console.log("Listening on 8080"))
